f8b55cd176c0da5ee7e1bc1352db5644e9c2a285
melvin <melvin.mark@centrino.co.ke>
2022-10-02 15:55:18 +0300

    feat: add pinia state events information

---
+++ src/interfaces/celeb-info.interface.ts
@@ -0,0 +1,6 @@
+export interface CelebsInterface {
+    type: string;
+    celeb_birthyear: number;
+    celeb_description: string;
+    celeb_bio: string;
+}
---
+++ src/stores/events-info.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import type {Events} from "@/interfaces/event-info.interface";
+
+export const useBirthdayInfo = defineStore('birthdayInfo', {
+    state: () => ({
+        /** @type {{ date: number, month: string} */
+        events: [] as Events[],
+
+    }),
+    getters: {
+        getEvents(state) {
+
+            return state.events;
+        },
+
+    },
+    actions: {
+
+        setBirthdayInfo(events: Events) {
+            this.events.push(events)
+        },
+    },
+})
